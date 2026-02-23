ef84ec20f458c1890542893d1841a5a1f906cb88
oskar089 <46764350+oskar089@users.noreply.github.com>
2026-02-23 21:42:10 +0100

    calculadora de edad

---
+++ Calculadora edad/app.js
@@ -0,0 +1,7 @@
+let anioNacimiento = 1989;
+
+let anioActual = 2026;
+
+let resultadoEdad = anioActual - anioNacimiento;
+
+console.log('Hola, Nací en ' + anioNacimiento + ' y en el año ' + anioActual + ' tengo ' + resultadoEdad);
