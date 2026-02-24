c7ee1ad33aa0aec5812b573e1af2b1d95477b5ee
oskar089 <46764350+oskar089@users.noreply.github.com>
2026-02-24 09:01:57 +0100

    semaforo

---
+++ Calculadora edad/semaforo.js
@@ -0,0 +1,10 @@
+let color = "rojo";
+if(color === "rojo"){
+    console.log("¡Para! 🛑")
+} else if (color === "amarillo"){
+    console.log("¡Precausion! ⚠️")
+} else if (color === "verde"){
+    console.log("¡Pasa! 🟢")
+}else{
+    console.log("¡Semaforo averiado! 🚫")
+}
