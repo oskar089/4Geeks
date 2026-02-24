f5a42039142a392c1fbe5a042ce1230c4ead3edd
oskar089 <46764350+oskar089@users.noreply.github.com>
2026-02-24 08:54:18 +0100

    temperatura

---
+++ Calculadora edad/aviso_temperatura.js
@@ -0,0 +1,8 @@
+let celsius = 25;
+if (celsius > 30){
+    console.log( "¡Uff, qué calor! Ponte Protector solar ☀️");
+}else if (celsius < 10){
+   console.log("Hace frio abrigate bien ❄️");
+}else {
+    console.log("El clima esta de maravilla ☁️");
+}
---
+++ Calculadora edad/conversor_temperatura.js
@@ -0,0 +1,5 @@
+let celsius = 25;
+
+let fahrenheit = (celsius * 1.8) + 32;
+
+console.log(celsius + "°C equivalen a " + fahrenheit);
